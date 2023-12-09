4aad7c57bed94a9f3df0598d4d5fc2c6cdd52f9d
javier <nhat1234559@gmail.com>
2023-12-09 19:36:04 +0700

    fix(seeder): remove color lib

--- backend/src/seeder.ts
+++ backend/src/seeder.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import colors from 'colors';
 import users from './data/users';
 import products from './data/products';
 import User from './models/userModel';
@@ -27,10 +26,10 @@ const importData = async () => {
 
     await Product.insertMany(sampleProducts);
 
-    console.log('Data Imported!'.green.inverse);
+    console.log('Data Imported!');
     process.exit();
   } catch (error) {
-    console.error(`${error}`.red.inverse);
+    console.error(`${error}`);
     process.exit(1);
   }
 };
@@ -41,10 +40,10 @@ const destroyData = async () => {
     await Product.deleteMany();
     await User.deleteMany();
 
-    console.log('Data Destroyed!'.red.inverse);
+    console.log('Data Destroyed!');
     process.exit();
   } catch (error) {
-    console.error(`${error}`.red.inverse);
+    console.error(`${error}`);
     process.exit(1);
   }
 };
